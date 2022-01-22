dc57948e0b54463cb411121d1900e77e5b9b95f9
尹东方 <wb.yindongfang@mesg.corp.netease.com>
2022-01-22 17:24:52 +0800

    feat: add dependencies & demo

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Col, Input, Row } from 'antd';
+import { marked } from 'marked';
+import React, { useEffect, useRef, useState } from 'react';
+
+export function App() {
+  const [input, setInput] = useState('');
+
+  const containerRef = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    containerRef.current.innerHTML = marked.parse(input);
+  }, [input]);
+
+  return (
+    <Row style={{ height: '100%' }}>
+      <Col span={12} style={{ height: '100%' }}>
+        <Input.TextArea
+          style={{ height: '100%' }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </Col>
+      <Col span={12}>
+        <div ref={containerRef} />
+      </Col>
+    </Row>
+  );
+}
